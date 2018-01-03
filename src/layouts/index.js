952a56b8be6cc487ebb150441d17763818a16251
Aleš Trunda <alestrunda@gmail.com>
2018-01-03 15:05:33 +0100

    fixed favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,8 +19,8 @@ const TemplateWrapper = ({ children }) => (
       <meta name="author" content="Aleš Trunda alestrunda.cz" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <link rel="shortcut icon" href="../../assets/favicon.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="favicon-152.png" />
+      <link rel="shortcut icon" href={require("../../assets/favicon.png")} />
+      <link rel="apple-touch-icon" sizes="152x152" href={require("../../assets/favicon-152.png")} />
 
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400,400i,600i,700,700i|Roboto+Condensed:300,400,700" rel="stylesheet" />
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
