a4e2591919f6fe2f71f3a9b8f4ab60f977a1edf7
Aleš Trunda <alestrunda@gmail.com>
2018-01-04 16:37:32 +0100

    Pane propTypes

--- src/components/Pane.js
+++ src/components/Pane.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Pane = (props) => {
     return <div>{props.children}</div>
 }
+
+Pane.propTypes = {
+    children: PropTypes.element.isRequired
+};
 
 export default Pane;
