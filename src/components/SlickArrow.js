ec8f984949e0c19d2d060346315b630ce98219f8
Aleš Trunda <alestrunda@gmail.com>
2018-01-04 17:12:05 +0100

    SlickArrow propTypes

--- src/components/SlickArrow.js
+++ src/components/SlickArrow.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const SlickArrow = (props) => {
 	const {className, onClick, children} = props
 	return <div className={className} onClick={onClick}>{children}</div>
+}
+
+SlickArrow.propTypes = {
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    children: PropTypes.element,
 }
 
 export default SlickArrow;
