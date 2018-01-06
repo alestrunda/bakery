7be679729be2df57cff42baf1b9b234702561476
Aleš Trunda <alestrunda@gmail.com>
2018-01-07 00:05:34 +0100

    load posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,8 +7,35 @@ const PageBlog = ({ data }) => (
     <Helmet title="Blog" />
 
     <h1>Blog</h1>
-    <p>content</p>
+    {data.posts.edges.map(({ node }, index) => {
+      return (
+        <div className="article">
+          <h1>{node.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: node.html }} />
+        </div>
+      )
+    })}
   </div>
 )
+
+export const query = graphql`
+  query PostsQuery {
+    posts: allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/posts/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          html
+        }
+      }
+    }
+  }
+`
 
 export default PageBlog
