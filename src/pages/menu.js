c5e34b6f5947cab9981ce2827ca8bf04efddfaea
Aleš Trunda <alestrunda@gmail.com>
2018-01-26 00:17:03 +0100

    page menu sample content

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -7,7 +7,17 @@ const PageMenu = ({ data }) => (
     <Helmet title="Menu" />
 
     <h1 className="heading-main-single">Menu</h1>
-    <p>content</p>
+    <p>Sed ullamcorper ac eros in maximus. Nam quis magna fringilla, volutpat quam in, eleifend quam. Integer bibendum odio sit amet lectus rhoncus, et hendrerit diam facilisis. Integer ultrices elit sit amet magna laoreet, vel sodales dolor sollicitudin. Sed ultricies aliquet egestas. Proin at risus eget nulla ullamcorper scelerisque.</p>
+    <ul>
+      <li>Quisque tempus</li>
+      <li>Magna eget</li>
+      <li>Eros posuere</li>
+      <li>Sed ac faucibus neque</li>
+      <li>Cras</li>
+      <li>Maecenas</li>
+      <li>Etiam eget</li>
+      <li>Nunc sagittis</li>
+    </ul>
   </div>
 )
 
