0fa4d63d884c28206e7505e2a93333aa418d181a
Aleš Trunda <alestrunda@gmail.com>
2018-02-01 23:39:16 +0100

    add Breadcrumbs for recipe pages

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -4,46 +4,54 @@ import Helmet from 'react-helmet'
 import FontAwesome from 'react-fontawesome'
 
 import ArticlePreview from '../components/ArticlePreview'
+import Breadcrumbs from '../components/Breadcrumbs'
 import HeadingMain from '../components/HeadingMain'
 
-const Recipes = ({ data }) => (
-  <div>
-    <Helmet title="Recipes" />
-    <HeadingMain title={`Recipes (${data.allMarkdownRemark.totalCount})`} />
+const Recipes = ({ data }) => {
+  const pageTitle = "Recipes"
+  return (
+    <div>
+      <Helmet title={pageTitle} />
+      <HeadingMain title={`${pageTitle} (${data.allMarkdownRemark.totalCount})`} />
 
-    <div className="section-content container">
-      {data.allMarkdownRemark.edges.map(({ node }, index) => {
-        return (
-          <ArticlePreview
-            key={node.id}
-            title={node.frontmatter.title}
-            textAlign="left"
-            layout="left"
-            imageShadow
-            imageSrc={
-              node.frontmatter.imageSrc &&
-              node.frontmatter.imageSrc.childImageSharp.responsiveSizes.src
-            }
-            link={node.fields.slug}
-          >
-            <h4 className="mb10 mt25">
-              <FontAwesome className="text-red text-bigger mr5" name="star" />
-              Ingredients
-            </h4>
-            <div
-              className="mb25 text-italic"
-              dangerouslySetInnerHTML={{ __html: node.frontmatter.ingredients }}
-            />
-            {node.excerpt}
-          </ArticlePreview>
-        )
-      })}
-      <Link to="/" className="button button--xsmall button--brown">
-        Back to the homepage
-      </Link>
+      <div className="container">
+        <Breadcrumbs items={[{ text: 'Home', to: '/' }, { text: pageTitle }]} />
+      </div>
+
+      <div className="section-content container">
+        {data.allMarkdownRemark.edges.map(({ node }, index) => {
+          return (
+            <ArticlePreview
+              key={node.id}
+              title={node.frontmatter.title}
+              textAlign="left"
+              layout="left"
+              imageShadow
+              imageSrc={
+                node.frontmatter.imageSrc &&
+                node.frontmatter.imageSrc.childImageSharp.responsiveSizes.src
+              }
+              link={node.fields.slug}
+            >
+              <h4 className="mb10 mt25">
+                <FontAwesome className="text-red text-bigger mr5" name="star" />
+                Ingredients
+              </h4>
+              <div
+                className="mb25 text-italic"
+                dangerouslySetInnerHTML={{ __html: node.frontmatter.ingredients }}
+              />
+              {node.excerpt}
+            </ArticlePreview>
+          )
+        })}
+        <Link to="/" className="button button--xsmall button--brown">
+          Back to the homepage
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export const query = graphql`
   query RecipesListQuery {
--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import FontAwesome from 'react-fontawesome'
 
+import Breadcrumbs from '../components/Breadcrumbs'
 import ImageLightbox from '../components/ImageLightbox'
 
 const PageRecipe = ({ data }) => {
@@ -16,51 +17,61 @@ const PageRecipe = ({ data }) => {
   } = post.frontmatter
   console.log(post.frontmatter.imageSrc.childImageSharp)
   return (
-    <div className="container section-content">
-      <Helmet title={title} />
+    <div className="container">
+      <Breadcrumbs
+        items={[
+          { text: 'Home', to: '/' },
+          { text: 'Blog', to: '/blog/' },
+          { text: title },
+        ]}
+      />
 
-      <div className="article">
-        <h1 className="mb5">{title}</h1>
-        <p className="text-small text-silver">{date}</p>
-        <p className="mb40">{`"${description}"`}</p>
-        <div className="grid mb40">
-          <div className="grid__item grid__item--md-span-6">
-            <h4 className="mb10">
-              <FontAwesome className="text-red text-bigger mr5" name="star" />
-              Ingredients:
-            </h4>
-            <div
-              className="mb30 text-italic"
-              dangerouslySetInnerHTML={{ __html: ingredients }}
-            />
-            <div className="text-big">
-              <span className="text-red-dark">PREP:</span>{' '}
-              <strong>{timePrep}min</strong>
-              <br />
-              <span className="text-red-dark">COOK:</span>{' '}
-              <strong>{timeCook}min</strong>
-              <br />
-              <span className="text-red-dark">READY IN:</span>{' '}
-              <strong>{timePrep + timeCook}min</strong>
+      <div className="section-content">
+        <Helmet title={title} />
+
+        <div className="article">
+          <h1 className="mb5">{title}</h1>
+          <p className="text-small text-silver">{date}</p>
+          <p className="mb40">{`"${description}"`}</p>
+          <div className="grid mb40">
+            <div className="grid__item grid__item--md-span-6">
+              <h4 className="mb10">
+                <FontAwesome className="text-red text-bigger mr5" name="star" />
+                Ingredients:
+              </h4>
+              <div
+                className="mb30 text-italic"
+                dangerouslySetInnerHTML={{ __html: ingredients }}
+              />
+              <div className="text-big">
+                <span className="text-red-dark">PREP:</span>{' '}
+                <strong>{timePrep}min</strong>
+                <br />
+                <span className="text-red-dark">COOK:</span>{' '}
+                <strong>{timeCook}min</strong>
+                <br />
+                <span className="text-red-dark">READY IN:</span>{' '}
+                <strong>{timePrep + timeCook}min</strong>
+              </div>
+            </div>
+            <div className="grid__item grid__item--md-span-6 grid__item--break-sm-30">
+              <ImageLightbox
+                previewImages={[
+                  post.frontmatter.imageSrc.childImageSharp.responsiveSizes.src,
+                ]}
+                fullImages={[
+                  post.frontmatter.imageSrc.childImageSharp.responsiveSizes
+                    .originalImg,
+                ]}
+              />
             </div>
           </div>
-          <div className="grid__item grid__item--md-span-6 grid__item--break-sm-30">
-            <ImageLightbox
-              previewImages={[
-                post.frontmatter.imageSrc.childImageSharp.responsiveSizes.src,
-              ]}
-              fullImages={[
-                post.frontmatter.imageSrc.childImageSharp.responsiveSizes
-                  .originalImg,
-              ]}
-            />
-          </div>
+          <h4 className="mb10">
+            <FontAwesome className="text-red text-bigger mr5" name="star" />
+            Steps:
+          </h4>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
-        <h4 className="mb10">
-          <FontAwesome className="text-red text-bigger mr5" name="star" />
-          Steps:
-        </h4>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </div>
   )
