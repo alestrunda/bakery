2ec112d3e7b52c98ae3407846f483c4bb2262d54
Aleš Trunda <alestrunda@gmail.com>
2020-10-25 21:01:42 +0100

    code cleaning

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -9,52 +9,46 @@ import Layout from '../layouts/Page'
 
 const PAGE_TITLE = 'Recipes'
 
-const Recipes = ({ data }) => {
-  return (
-    <Layout title={PAGE_TITLE}>
-      <HeadingMain
-        title={`${PAGE_TITLE} (${data.allMarkdownRemark.totalCount})`}
-      />
-
-      <div className="container">
-        <Breadcrumbs
-          items={[{ text: 'Home', to: '/' }, { text: PAGE_TITLE }]}
-        />
-      </div>
-
-      <div className="section-content section-content--top-smaller container">
-        {data.allMarkdownRemark.edges.map(({ node }, index) => {
-          return (
-            <ArticlePreview
-              key={node.id}
-              title={node.frontmatter.title}
-              textAlign="left"
-              layout="left"
-              imageShadow
-              imageSrc={
-                node.frontmatter.imageSrc &&
-                node.frontmatter.imageSrc.childImageSharp.fluid.src
-              }
-              link={node.fields.slug}
-            >
-              <h4 className="mb10 mt25">
-                <FontAwesome className="text-red text-bigger mr5" name="star" />
-                Ingredients
-              </h4>
-              <div className="mb25 text-italic">
-                {node.frontmatter.ingredients.join(', ')}
-              </div>
-              {node.excerpt}
-            </ArticlePreview>
-          )
-        })}
-        <Link to="/" className="button button--xsmall button--brown">
-          Back to the homepage
-        </Link>
-      </div>
-    </Layout>
-  )
-}
+const Recipes = ({ data }) => (
+  <Layout title={PAGE_TITLE}>
+    <HeadingMain
+      title={`${PAGE_TITLE} (${data.allMarkdownRemark.totalCount})`}
+    />
+    <div className="container">
+      <Breadcrumbs items={[{ text: 'Home', to: '/' }, { text: PAGE_TITLE }]} />
+    </div>
+    <div className="section-content section-content--top-smaller container">
+      {data.allMarkdownRemark.edges.map(({ node }, index) => {
+        return (
+          <ArticlePreview
+            key={node.id}
+            title={node.frontmatter.title}
+            textAlign="left"
+            layout="left"
+            imageShadow
+            imageSrc={
+              node.frontmatter.imageSrc &&
+              node.frontmatter.imageSrc.childImageSharp.fluid.src
+            }
+            link={node.fields.slug}
+          >
+            <h4 className="mb10 mt25">
+              <FontAwesome className="text-red text-bigger mr5" name="star" />
+              Ingredients
+            </h4>
+            <div className="mb25 text-italic">
+              {node.frontmatter.ingredients.join(', ')}
+            </div>
+            {node.excerpt}
+          </ArticlePreview>
+        )
+      })}
+      <Link to="/" className="button button--xsmall button--brown">
+        Back to the homepage
+      </Link>
+    </div>
+  </Layout>
+)
 
 export const query = graphql`
   query RecipesListQuery {
